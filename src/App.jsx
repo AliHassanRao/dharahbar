d0e4f743e597034c5ec914c2431699c516c26dd1
Engineer Ali <157731592+AliHassanRao@users.noreply.github.com>
2025-08-09 07:53:47 +0500

    kk

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-// App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./Layout"; 
+
+import Navbar from "./Components/Navbar";
+import DreamSection from "./Components/DreamSection";
+import ExploreLife from "./Components/ExploreLife";
+import Footer from "./Components/Footer";
+import DirectorMessage from "./Components/DirectorMessage";
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-     
-        </Route>
-      </Routes>
-    </Router>
+   <div className="min-h-screen bg-gray-100 text-gray-900">
+      <Navbar />
+      <DreamSection />
+      <DirectorMessage/>
+      <ExploreLife />
+      <Footer />
+    </div>
   );
 }
 
