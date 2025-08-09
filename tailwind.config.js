668f8dfb69861c29234b5affb613047138c37f54
Engineer Ali <157731592+AliHassanRao@users.noreply.github.com>
2025-08-09 07:39:43 +0500

    first

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+import { defineConfig } from 'tailwindcss';
+
+export default defineConfig({
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif'], // Rubik as the default sans font
+      },
+    },
+  },
+  plugins: [],
+});
