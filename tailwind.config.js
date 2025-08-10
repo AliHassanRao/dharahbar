d900d8d09ac65352eb0ab332df38c89ff8be5c72
Engineer Ali <157731592+AliHassanRao@users.noreply.github.com>
2025-08-10 06:55:18 +0500

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
-// tailwind.config.js
 import { defineConfig } from 'tailwindcss';
 
 export default defineConfig({
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Rubik', 'sans-serif'], // Rubik as the default sans font
+        sans: ['Rubik', 'sans-serif'],
       },
+      
     },
   },
   plugins: [],
